f5aa3d29a84c5d98296d2abd182770ca3eaffbf7
卢思侗 <wowlusitong@gmail.com>
2019-03-24 18:01:36 +0800

    feat(korok): 空列表可以使用delete删除列表样式

--- packages/core/src/scripts/events/keydown/Backspace.js
+++ packages/core/src/scripts/events/keydown/Backspace.js
@@ -2,6 +2,14 @@ import command from '~/commands';
 
 export default function({ editor, next }) {
   const focusBlock = editor.value.focusBlock;
+  if (focusBlock.text === '' && focusBlock.type === 'listitem') {
+    event.preventDefault();
+    editor
+      .setBlocks('paragraph')
+      .unwrapBlock('unorderedlist')
+      .unwrapBlock('orderedlist');
+    return;
+  }
   if (focusBlock.text === '' && focusBlock.type !== 'paragraph') {
     event.preventDefault();
     command(editor)('paragraph');
