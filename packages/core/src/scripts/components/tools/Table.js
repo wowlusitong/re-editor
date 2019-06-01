f2341b0a4496103c5420840ceaab06e7a480382e
卢思侗 <wowlusitong@gmail.com>
2019-06-01 12:07:38 +0800

    feat(core): 优化表格增加行/列的操作

--- packages/core/src/scripts/components/tools/Table.js
+++ packages/core/src/scripts/components/tools/Table.js
@@ -2,64 +2,66 @@ import React from 'react';
 
 import Icon from '~/components/utils/Icon';
 import Item from '~/components/utils/ToolItem';
+import Table from '~/utils/table';
 
-export default class Table extends React.Component {
+export default class TableTool extends React.Component {
   handleDelete = event => {
     event.stopPropagation();
     const { editor, node } = this.props;
     editor.removeNodeByKey(node.key);
   };
 
-  handleInsertColumn = event => {
-    event.stopPropagation();
+  handleInsertBelowRow = () => {
     const { editor } = this.props;
-    editor.insertBlock('td');
+    new Table(editor).insertRow('below');
   };
 
-  handleInsertRow = event => {
-    event.stopPropagation();
-    const { editor, node } = this.props;
-    const tbody = node.getDescendant([0]);
-    editor.insertNodeByKey(tbody.key, tbody.nodes.size, {
-      object: 'block',
-      type: 'tr',
-      nodes: Array(node.getDescendant([0, 0]).nodes.size).fill({
-        object: 'block',
-        type: 'td'
-      })
-    });
-  };
-
-  handleRemoveRow = event => {
-    event.stopPropagation();
-    const { editor, node } = this.props;
-    const focusRow = node
-      .getDescendant([0])
-      .nodes.find(node => node.hasDescendant(editor.value.focusBlock.key));
-    editor.removeNodeByKey(focusRow.key);
-  };
-
-  handleRemoveColumn = event => {
-    event.stopPropagation();
+  handleInsertAboveRow = () => {
     const { editor } = this.props;
-    editor.removeNodeByKey(editor.value.focusBlock.key);
+    new Table(editor).insertRow('above');
+  };
+
+  handleRemoveRow = () => {
+    const { editor } = this.props;
+    new Table(editor).removeFocusRow();
+  };
+
+  handleRemoveColumn = () => {
+    const { editor } = this.props;
+    new Table(editor).removeFocusColumn();
+  };
+
+  handleInsertRightCloumn = () => {
+    const { editor } = this.props;
+    new Table(editor).insertColumn('right');
+  };
+
+  handleInsertLeftCloumn = () => {
+    const { editor } = this.props;
+    new Table(editor).insertColumn('left');
   };
 
   render() {
     return (
       <>
-        <Item hover tip="删除选中行" onClick={this.handleRemoveColumn}>
-          <Icon type="icon-remove-column" />
-        </Item>
-        <Item hover tip="增加列" onClick={this.handleInsertColumn}>
-          <Icon type="icon-insert-column" />
-        </Item>
-        <Item.Split />
         <Item hover tip="删除选中行" onClick={this.handleRemoveRow}>
           <Icon type="icon-remove-row" />
         </Item>
-        <Item hover tip="增加行" onClick={this.handleInsertRow}>
+        <Item hover tip="上方增加行" onClick={this.handleInsertAboveRow}>
           <Icon type="icon-insert-row" />
+        </Item>
+        <Item hover tip="下方增加行" onClick={this.handleInsertBelowRow}>
+          <Icon type="icon-InsertRowAfter" />
+        </Item>
+        <Item.Split />
+        <Item hover tip="删除选中列" onClick={this.handleRemoveColumn}>
+          <Icon type="icon-remove-column" />
+        </Item>
+        <Item hover tip="左方增加列" onClick={this.handleInsertLeftCloumn}>
+          <Icon type="icon-insert-column" />
+        </Item>
+        <Item hover tip="右方增加列" onClick={this.handleInsertRightCloumn}>
+          <Icon type="icon-InsertColumnAfter" />
         </Item>
         <Item.Split />
         <Item hover tip="删除" onClick={this.handleDelete}>
---
+++ packages/core/src/scripts/utils/table.js
@@ -0,0 +1,54 @@
+export default class Table {
+  constructor(editor) {
+    this.editor = editor;
+    const focusKey = editor.value.selection.focus.key;
+    this.table = editor.value.document.nodes.find(node =>
+      node.hasDescendant(focusKey)
+    );
+    this.tbody = this.table.nodes.first();
+    this.trs = this.tbody.nodes;
+    this.focusTr = this.trs.find(tr => tr.hasDescendant(focusKey));
+    this.focusTrIndex = this.trs.findIndex(tr => tr.hasDescendant(focusKey));
+    this.focusTdIndex = this.focusTr.nodes.findIndex(td =>
+      td.hasDescendant(focusKey)
+    );
+  }
+
+  removeFocusColumn() {
+    this.trs.forEach(tr => {
+      this.editor.removeNodeByKey(tr.nodes.get(this.focusTdIndex).key);
+    });
+  }
+
+  insertColumn(direction = 'right') {
+    this.trs.forEach(tr => {
+      const td = tr.nodes.get(this.focusTdIndex);
+      this.editor.moveToRangeOfNode(td);
+      if (direction === 'right') {
+        this.editor.moveToEnd();
+      } else {
+        this.editor.moveToStart();
+      }
+      this.editor.insertBlock('td');
+    });
+  }
+
+  removeFocusRow() {
+    this.editor.removeNodeByKey(this.focusTr.key);
+  }
+
+  insertRow(direction = 'below') {
+    this.editor.insertNodeByKey(
+      this.tbody.key,
+      this.focusTrIndex + (direction === 'below' ? 1 : 0),
+      {
+        object: 'block',
+        type: 'tr',
+        nodes: Array(this.focusTr.nodes.size).fill({
+          object: 'block',
+          type: 'td'
+        })
+      }
+    );
+  }
+}
